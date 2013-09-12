02312036639ddc32549836c4c8b9df6e0243b76f
Rodrigo Deodoro <roddds@gmail.com>
2013-09-12 00:13:08 -0300

    removed superfluous view

--- owned/apps/core/urls.py
+++ owned/apps/core/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from core.views import HomeView, LoginView
+from core.views import HomeView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
--- owned/urls.py
+++ owned/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
+
 
 urlpatterns = patterns('',
     
     url(r'^', include('core.urls')),
-    # url(r'game/$', include('game.urls')),
+    url(r'^', include('core.auth_urls')),
     url(r'^book/', include('book.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
