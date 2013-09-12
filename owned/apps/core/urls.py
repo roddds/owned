e7d22c380d0dba8ca2ab0fa744235c17e8f35a82
Rodrigo Deodoro <roddds@gmail.com>
2013-09-12 00:12:13 -0300

    replaced homepage view

--- owned/apps/core/urls.py
+++ owned/apps/core/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from core.views import HomeView, LoginView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
-    url(r'^$', 'core.views.home', name='home'),
+    url(r'^$', HomeView.as_view()),
 )
--- owned/apps/core/views/home.py
+++ owned/apps/core/views/home.py
@@ -1,6 +1,18 @@
-from annoying.decorators import render_to
+from django.views.generic.base import View, TemplateResponseMixin
 
 
-@render_to('home.html')
-def home(request):
-    return {}
+class HomeView(TemplateResponseMixin, View):
+    template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['user_authenticated'] = request.user.is_authenticated()
+        return self.render_to_response(context)
+
+
+class LoginView(View):
+    def post(self, request, *args, **kwargs):
+        context = {}
+        import ipdb; ipdb.set_trace()
+        return self.render_to_response(context)
+
