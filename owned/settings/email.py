18d292419167d555fcc926e6303035f69d7f2eb3
Rodrigo Deodoro <roddds@gmail.com>
2013-09-14 13:49:56 -0300

    removed debug code

--- owned/settings/email.py
+++ owned/settings/email.py
@@ -9,9 +9,10 @@ EMAIL_HOST_PASSWORD=os.environ.get("RODRIGO_PASSWORD")
 
 DEFAULT_FROM_EMAIL = 'Rodrigo Deodoro <roddds@gmail.com>'
 
-EMAIL_HOST="localhost"
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = 'testing@example.com'
+## debug code
+# EMAIL_HOST="localhost"
+# EMAIL_PORT = 1025
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
+# EMAIL_USE_TLS = False
+# DEFAULT_FROM_EMAIL = 'testing@example.com'
