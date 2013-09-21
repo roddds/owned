6c22793cc0516709c8db8a0a4f185297923cd094
Rodrigo Deodoro <roddds@gmail.com>
2013-09-21 02:40:48 -0300

    game app urls.py file

---
+++ owned/apps/game/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from game.views import NewGameView, ContinueGameView, PlayChapterView
+
+urlpatterns = patterns('',
+    url(r'^new$', NewGameView.as_view()),
+    url(r'^continue$', ContinueGameView.as_view()),
+
+    url(r'^chapter/(?P<chapter>\d+)', PlayChapterView.as_view()),
+)
