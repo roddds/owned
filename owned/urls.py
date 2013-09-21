0774711fdfb041cb02d7aaa771182c9d99a33883
Rodrigo Deodoro <roddds@gmail.com>
2013-09-21 02:40:31 -0300

    url for new game views

--- owned/urls.py
+++ owned/urls.py
@@ -1,12 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    
+
     url(r'^', include('core.urls')),
     url(r'^', include('core.auth_urls')),
     url(r'^book/', include('book.urls')),
+    url(r'^game/', include('game.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^new_game$', RedirectView.as_view(url="game/new")),
+    url(r'^continue$', RedirectView.as_view(url="game/continue"))
+
 )
