4a475fc712be5c289e1169b8952be596335597d4
Rodrigo Deodoro <roddds@gmail.com>
2013-09-21 02:43:26 -0300

    game initial views

--- owned/apps/game/views.py
+++ owned/apps/game/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
+from django.contrib import auth
+
+
+class BaseGameView(TemplateView):
+    def get_context(self):
+        cxt = {}
+
+
+class NewGameView(TemplateView):
+    template_name = "new_game.html"
+
+
+    def get(self, *args, **kwargs):
+        import ipdb; ipdb.set_trace()
+        return self.render_to_response({})
+
+
+class ContinueGameView(TemplateView):
+    pass
+
+
+class PlayChapterView(TemplateView):
+    pass
