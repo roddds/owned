1b928c7b71a8596a994af5742f1e02df9a42fdd6
Rodrigo Deodoro <roddds@gmail.com>
2013-09-21 19:20:40 -0300

    removed superfluous import

--- owned/apps/core/urls.py
+++ owned/apps/core/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 from core.views import HomeView
-from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
     url(r'^$', HomeView.as_view()),
