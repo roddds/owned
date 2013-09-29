30bea3fbe56042348e94577f8c66b78fe236daef
Rodrigo Deodoro <roddds@gmail.com>
2013-09-28 22:52:04 -0300

    added continue view

--- owned/apps/game/urls.py
+++ owned/apps/game/urls.py
@@ -4,6 +4,7 @@ from game.views import NewGameView, ContinueGameView, PlayChapterView
 urlpatterns = patterns('',
     url(r'^new$', NewGameView.as_view()),
     url(r'^continue$', ContinueGameView.as_view()),
+    url(r'^continue/(?P<slot>\d+)', NewGameView.as_view()),
 
     url(r'^chapter/(?P<chapter>\d+)', PlayChapterView.as_view()),
 )
