735f36740dfce16e72b1de3281580475430af210
Rodrigo Deodoro <roddds@gmail.com>
2013-09-28 22:44:41 -0300

    added basic CBV for reading book

--- owned/apps/book/views.py
+++ owned/apps/book/views.py
@@ -2,23 +2,22 @@ from django.views.generic import TemplateView
 from book.models import Paragraph
 
 
-class ReadChapter(TemplateView):
-    template_name = 'read.html'
-
+class BaseBookView(TemplateView):
     def get_context(self):
         cxt = {}
         cxt['paragraph'] = Paragraph.objects.get(pk=self.kwargs['chapter'])
         cxt['options'] = cxt['paragraph'].option_set.all()
+        cxt['player'] = self.request.user.player
+        cxt['saveslot'] = self.request.user.player.saveslot
 
         return cxt
 
+
+class ReadChapter(BaseBookView):
+    template_name = 'read.html'
+
     def get(self, request, **kwargs):
         cxt = self.get_context()
+
+
         return self.render_to_response(cxt)
-
-
-# @render_to('read.html')
-# def home(request):
-#     p = Paragraph.objects.get(pk=1)
-#     return {'paragraph': p,
-#             'options': p.option_set.all()}
