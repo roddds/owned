904b33af56fbb4e754ca4e140699d6a51295c364
Rodrigo Deodoro <roddds@gmail.com>
2013-10-12 13:03:02 -0300

    added book's admin.py

--- owned/apps/book/admin.py
+++ owned/apps/book/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
+from django.db import models
 from book.models import Paragraph, Option
 
+class OptionAdmin(admin.StackedInline):
+    model = Option
 
 class ParagraphAdmin(admin.ModelAdmin):
     list_display = ('title', 'display_text')
 
+    inlines = [OptionAdmin,]
+
 
 admin.site.register(Paragraph, ParagraphAdmin)
 admin.site.register(Option)
+
