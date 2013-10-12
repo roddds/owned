594604f0ece62a1d44f707b881021ef32be1d490
Rodrigo Deodoro <roddds@gmail.com>
2013-10-12 13:07:38 -0300

    urls for new game and continue views

--- owned/apps/game/urls.py
+++ owned/apps/game/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import patterns, include, url
 from game.views import NewGameView, ContinueGameView, PlayChapterView
 
 urlpatterns = patterns('',
-    url(r'^new$', NewGameView.as_view()),
-    url(r'^continue$', ContinueGameView.as_view()),
-    url(r'^continue/(?P<slot>\d+)', NewGameView.as_view()),
+    url(r'^new$', NewGameView.as_view(), name="new-game-select"),
+    url(r'^continue$', ContinueGameView.as_view(), name="continue-game-select"),
 
-    url(r'^chapter/(?P<chapter>\d+)', PlayChapterView.as_view()),
+    url(r'^continue/(?P<chapter>\d+)', PlayChapterView.as_view(), name="play-chapter"),
 )
