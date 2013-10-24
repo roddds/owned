d0a2e49b374a94bd7e14a76320f95ae669cc64ca
Rodrigo Deodoro <roddds@gmail.com>
2013-10-24 03:13:47 -0200

    mail settings using mailjet

--- owned/settings/email.py
+++ owned/settings/email.py
@@ -2,18 +2,18 @@ import os
 
 ACCOUNT_ACTIVATION_DAYS=7
 
-EMAIL_HOST="localhost"
-EMAIL_PORT = 1025
-EMAIL_USE_TLS = False
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
-DEFAULT_FROM_EMAIL = 'testing@example.com'
-
-if os.environ.get("DJANGO_ENV", "local"):
-    EMAIL_HOST='smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_SERVER='roddds@gmail.com'
+if os.environ.get('DJANGO_ENV', 'local') == 'production':
+    EMAIL_HOST = "in.mailjet.com"
+    EMAIL_HOST_USER = os.environ.get("MAILJET_USERNAME")
+    EMAIL_HOST_PASSWORD = os.environ.get("MAILJET_PASSWORD")
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER='roddds@gmail.com'
-    EMAIL_HOST_PASSWORD=os.environ.get("RODRIGO_PASSWORD")
-    DEFAULT_FROM_EMAIL = 'Rodrigo Deodoro <roddds@gmail.com>'
+    EMAIL_PORT = 587
+    DEFAULT_FROM_EMAIL = 'Owned - Um Novo Jogador <owned@deodoro.org>'
+else:
+    EMAIL_HOST="localhost"
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
+    EMAIL_HOST_USER = ""
+    EMAIL_HOST_PASSWORD = ""
+    DEFAULT_FROM_EMAIL = 'testing@example.com'
+
