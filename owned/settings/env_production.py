d599ed26c1cf7631e066ffdf233a9addcdcb0295
Rodrigo Deodoro <roddds@gmail.com>
2013-10-25 01:43:23 -0200

    shuffling email settings around

--- owned/settings/env_production.py
+++ owned/settings/env_production.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 SERVE_MEDIA = DEBUG
+
+EMAIL_HOST = "in.mailjet.com"
+EMAIL_HOST_USER = os.environ.get("MAILJET_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILJET_PASSWORD")
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = 'Owned - Um Novo Jogador <owned@deodoro.org>'
