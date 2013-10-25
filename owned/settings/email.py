68c3fcc3e7b08d74a9c90f589afc0b9251774639
Rodrigo Deodoro <roddds@gmail.com>
2013-10-25 01:39:13 -0200

    shuffling email settings around

--- owned/settings/email.py
+++ owned/settings/email.py
@@ -1,19 +1,10 @@
 import os
 
-ACCOUNT_ACTIVATION_DAYS=7
+ACCOUNT_ACTIVATION_DAYS = 7
 
-if os.environ.get('DJANGO_ENV', 'local') == 'production':
-    EMAIL_HOST = "in.mailjet.com"
-    EMAIL_HOST_USER = os.environ.get("MAILJET_USERNAME")
-    EMAIL_HOST_PASSWORD = os.environ.get("MAILJET_PASSWORD")
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    DEFAULT_FROM_EMAIL = 'Owned - Um Novo Jogador <owned@deodoro.org>'
-else:
-    EMAIL_HOST="localhost"
-    EMAIL_PORT = 1025
-    EMAIL_USE_TLS = False
-    EMAIL_HOST_USER = ""
-    EMAIL_HOST_PASSWORD = ""
-    DEFAULT_FROM_EMAIL = 'testing@example.com'
-
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+DEFAULT_FROM_EMAIL = 'testing@example.com'
