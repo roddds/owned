97150874cdb63cd7b317ef121ea33dd271887cbd
Rodrigo Deodoro <roddds@gmail.com>
2019-11-17 14:48:46 -0800

    Updates

---
+++ precache-manifest.b559c313b8733fda6d43c08757d3fa4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1398151b5bcc53438d249eb1f2d15503",
+    "url": "/owned/index.html"
+  },
+  {
+    "revision": "31de8d539f512b1446d6",
+    "url": "/owned/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "918cf32615006df88a27",
+    "url": "/owned/static/css/main.d16dac35.chunk.css"
+  },
+  {
+    "revision": "31de8d539f512b1446d6",
+    "url": "/owned/static/js/2.b1fc4a73.chunk.js"
+  },
+  {
+    "revision": "918cf32615006df88a27",
+    "url": "/owned/static/js/main.82d9d6e3.chunk.js"
+  },
+  {
+    "revision": "81f9d474e64f45238036",
+    "url": "/owned/static/js/runtime-main.9f551ab1.js"
+  }
+]);
