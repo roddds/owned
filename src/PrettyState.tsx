4d86a64204d813fdd3fa8701fdd9ec578d0a5a29
Rodrigo Deodoro <roddds@gmail.com>
2019-11-19 23:37:04 -0800

    Prettier PrettyState

--- src/PrettyState.tsx
+++ src/PrettyState.tsx
@@ -4,8 +4,10 @@ import { ContextType } from './GameState';
 
 const PrettyState = ({ context }: { context: ContextType }) => {
   return (
-    <Box style={{ maxHeight: '400px', overflowY: 'scroll' }}>
-      <pre>{JSON.stringify(context, null, 2)}</pre>
+    <Box>
+      <pre style={{ maxHeight: '400px' }}>
+        {JSON.stringify(context, null, 2)}
+      </pre>
     </Box>
   );
 };
