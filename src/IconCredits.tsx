7b89c0838c8fe883c96bf6bd9cd39718b9ca7b15
Rodrigo Deodoro <roddds@gmail.com>
2019-11-19 22:47:32 -0800

    Get rid of ugly spaces

--- src/IconCredits.tsx
+++ src/IconCredits.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 
+const Flaticon = () => (
+  <a target='blank' rel='noopener noreferrer' href='https://www.flaticon.com/'>
+    Flaticon
+  </a>
+);
+
+const Icons8 = () => (
+  <a target='blank' rel='noopener noreferrer' href='https://icons8.com/icons'>
+    Icons8
+  </a>
+);
+
 const IconCredits = () => (
   <small
     style={{ display: 'block', opacity: '40%' }}
     className='is-unselectable'
   >
-    Icons by{' '}
-    <a
-      target='blank'
-      rel='noopener noreferrer'
-      href='https://www.flaticon.com/'
-    >
-      Flaticon
-    </a>{' '}
-    and{' '}
-    <a target='blank' rel='noopener noreferrer' href='https://icons8.com/icons'>
-      Icons8
-    </a>
+    Icons by <Flaticon /> and <Icons8 />
   </small>
 );
 
