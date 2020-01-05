301c7b0198cea7e07aadd3abe3c30d28e06979c4
Rodrigo Deodoro <roddds@gmail.com>
2020-01-05 03:15:25 -0800

    Add instructions to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Columns, Column, Button, Title } from 'bloomer';
+import { Container, Columns, Column, Content, Button, Title } from 'bloomer';
 import Sidebar from './Sidebar';
 import Game from './Game';
 import logo from './logo_flat.png';
@@ -21,9 +21,32 @@ const App = () => {
             hasTextAlign='centered'
             style={{ width: '100%' }}
           >
-            <Column>
+            <Column isSize={10} isOffset={1}>
               <img src={logo} alt='Owned logo' style={{ maxWidth: '50%' }} />
               <Title>Owned - Um Novo Jogador</Title>
+              <Content>
+                <p>
+                  <strong>OWNED - um novo jogador</strong> é um jogo interativo.
+                  Você decide o final.
+                </p>
+
+                <p>
+                  Em <strong>OWNED</strong> você é André, um técnico de
+                  informática viciado em videogames que vai tentar conquistar
+                  pelo menos uma dentre <strong>sete</strong> garotas. Para
+                  jogar, basta clicar em uma das opções no final de cada trecho
+                  de história, dando um rumo à vida de André.
+                </p>
+
+                <p>
+                  Você pode salvar o jogo e pode jogar de novo quantas vezes
+                  quiser. É grátis!
+                </p>
+
+                <p>
+                  <span>Recomendado para maiores de 18 anos.</span>
+                </p>
+              </Content>
               <Button
                 onClick={() => {
                   newGame();
